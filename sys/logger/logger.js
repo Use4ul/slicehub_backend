8853c6497faff4d56974d43856097acc98ba8fb2
Use4ul <i.am.use4ul@gmail.com>
2025-08-26 15:17:39 +0300

    первая настройка логера

---
+++ sys/logger/logger.js
@@ -0,0 +1,57 @@
+const log4js = require('log4js');
+const path = require('path');
+
+// default settings
+const TODAY = '_' + new Date().toISOString().split('T')[0].replaceAll('-', '_') + '.log';
+const type = 'file';
+const maxLogSize = 5242880;
+const backups = 7;
+const layoutType = 'pattern';
+const layoutPattern = '%d{|yyyy-MM-dd|hh:mm:ss.SSS|} [%p] %m';
+
+log4js.configure({
+    appenders: {
+        console: { type: 'console' },
+        main: {
+            type,
+            filename: path.join(__dirname, '../../logs/main' + TODAY),
+            maxLogSize,
+            backups,
+            layout: {
+                type: layoutType,
+                pattern: layoutPattern,
+            },
+        },
+        database: {
+            type,
+            filename: path.join(__dirname, '../../logs/dataBase' + TODAY),
+            maxLogSize,
+            backups,
+            layout: {
+                type: layoutType,
+                pattern: layoutPattern,
+            },
+        },
+        sync: {
+            type,
+            filename: path.join(__dirname, '../../logs/sync' + TODAY),
+            maxLogSize,
+            backups,
+            layout: {
+                type: layoutType,
+                pattern: layoutPattern,
+            },
+        },
+    },
+    categories: {
+        default: { appenders: ['console', 'main'], level: 'info' },
+        database: { appenders: ['database'], level: 'debug' },
+        sync: { appenders: ['sync', 'console'], level: 'info' },
+    },
+});
+
+module.exports = {
+    mainLogger: log4js.getLogger(),
+    dataBaseLogger: log4js.getLogger('database'),
+    syncLogger: log4js.getLogger('sync'),
+};
