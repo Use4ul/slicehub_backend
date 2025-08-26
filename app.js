effb22070caeb717ea3832592d27f793a6cbe7aa
Use4ul <i.am.use4ul@gmail.com>
2025-08-26 15:17:39 +0300

    первая настройка логера

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+const conf = require('./config.json');
 const express = require('express');
+const { mainLogger } = require('./sys/logger/logger');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = conf.app.port || gprocess.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const bootstrap = async () => {
     try {
-        startWebServer();
+        mainLogger.info(`Starting bootstrap func...`);
+        startApp();
+        mainLogger.info(`App is running...`);
     } catch (error) {
-        console.log('Application startup failed:', error);
+        mainLogger.error('APP STARTING ERROR:', error?.message);
     }
 };
 
-function startWebServer() {
+function startApp() {
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        mainLogger.info(`App port:: ${PORT}`);
     });
 }
 
-bootstrap().catch((e) => `Some error ${e}`);
+bootstrap().catch((e) => `SOME BOOTSTRAP ERROR ${e?.message}`);
